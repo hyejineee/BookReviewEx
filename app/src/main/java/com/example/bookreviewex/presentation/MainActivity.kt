f0bc75738c659e2798a682ba9c08bcf6b374eb0c
hyejineee <dev.hyejin04@gmail.com>
2021-10-29 17:01:05 +0900

    Review목록 보여주기 기능 완료
    이전의 클리아키텍처 투투리스트 만들기 수업을 들으면서 만든 프로젝트를 참고하여
    TDD방식으로 디비에서 리뷰데이터들을 가져와 목록으로 보여주는 기능을 구현했다.
    
    - 배웠던 방식을 적용하기 위해 라이브 데이터, 코루틴 등을 사용했다.
    - 프로젝트 폴더 구조를 presentation, domain, 외부 관련 으로 패키지를 나눠 진행했다.
    - di를 사용하기 위해 간단한 koin을 사용했다. 나중에 hilt로 업그레이드 할 수 있으면 좋겠다.
    
    다음으로 할 것
    - API를 사용하여 검색한 도서 가져오기

--- app/src/main/java/com/example/bookreviewex/presentation/MainActivity.kt
+++ app/src/main/java/com/example/bookreviewex/presentation/MainActivity.kt
@@ -2,7 +2,10 @@ package com.example.bookreviewex.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.bookreviewex.databinding.ActivityMainBinding
+import com.example.bookreviewex.presentation.viewmodel.ReviewListState
 import com.example.bookreviewex.repository.localdb.entity.ReviewEntity
 import com.example.bookreviewex.repository.service.model.BookDTO
 import com.example.bookreviewex.usecase.InsertReviewListUseCase
@@ -18,35 +21,60 @@ class MainActivity : AppCompatActivity() {
 
     private val insetReviewListUseCase : InsertReviewListUseCase by inject()
 
+    private val adapter = ReviewAdapter()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val mockReviewedBooks = listOf(
-            ReviewEntity(
-                1,
-                BookDTO(isbn = "1111",title = "title", image = "",description = "description" ),
-                "2021.10.26",
-                "content1"
-            ),
-            ReviewEntity(
-                2,
-                BookDTO(isbn = "2222", title = "title2", image = "",description = "description" ),
-                "2021.10.26",
-                "content2"
-            )
-        )
+        attachListView()
+        observeData()
 
-        GlobalScope.launch {
-            insetReviewListUseCase(mockReviewedBooks)
-        }
-
-        reviewListViewModel.fetchAllReview()
     }
+
+    private fun attachListView(){
+        binding.bookList.layoutManager = LinearLayoutManager(this)
+        binding.bookList.adapter = adapter
+        adapter.submitList(null)
+
+    }
+
+    private fun observeData(){
+        reviewListViewModel.reviewList.observe(this){
+            Log.d(TAG, "state : ${it}")
+
+            when(it){
+                is ReviewListState.Error -> handleErrorState()
+                is ReviewListState.Idle -> attachListView()
+                is ReviewListState.Loading -> handleLoadingState()
+                is ReviewListState.Success -> handleSuccessState(it)
+            }
+
+
+        }
+    }
+
+    private fun handleErrorState(){
+
+    }
+
+    private fun handleLoadingState(){
+
+    }
+
+    private fun handleSuccessState(state:ReviewListState.Success){
+        adapter.submitList(state.reviewList.toMutableList())
+    }
+
+
 
     private val handleSearchButton = {
         val keyword = binding.searchEditTextView.text
         //todo 도서 검색 api로 요청하기
+    }
+
+    companion object{
+        private const val TAG = "ReviewListActivity"
     }
 }
--- app/src/main/java/com/example/bookreviewex/presentation/ReviewAdapter.kt
+++ app/src/main/java/com/example/bookreviewex/presentation/ReviewAdapter.kt
@@ -1,5 +1,43 @@
 package com.example.bookreviewex.presentation
 
-class ReviewAdapter(){
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.bookreviewex.databinding.ItemReviewBinding
+import com.example.bookreviewex.repository.localdb.entity.ReviewEntity
+
+class ReviewAdapter(
+) : ListAdapter<ReviewEntity, ReviewAdapter.ViewHolder>(DiffCallback) {
+
+    inner class ViewHolder(private val binding: ItemReviewBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(reviewItem:ReviewEntity) {
+            binding.contentTextView.text = reviewItem.content
+            binding.titleTextView.text = reviewItem.book.title
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(ItemReviewBinding.inflate(LayoutInflater.from(parent.context) ,parent, false))
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+
+    companion object {
+        object DiffCallback : DiffUtil.ItemCallback<ReviewEntity>() {
+            override fun areItemsTheSame(oldItem: ReviewEntity, newItem: ReviewEntity): Boolean {
+                return oldItem.id == newItem.id
+            }
+
+            override fun areContentsTheSame(oldItem: ReviewEntity, newItem: ReviewEntity): Boolean {
+                return oldItem == newItem
+            }
+
+        }
+    }
 
 }
