5ee1800eccf02640929e1e5253cad94207c27780
hyejineee <dev.hyejin04@gmail.com>
2021-11-03 15:13:10 +0900

    검색한 책 리스트, 리뷰 리스트 화면 정리
    - 리싸이클러뷰 두개로 분리
    - 검색 텍스트뷰 - 텍스트 체인지 리스너 추가
    - 리스트뷰 visible변경 코드 추가

--- app/src/main/java/com/example/bookreviewex/presentation/MainActivity.kt
+++ app/src/main/java/com/example/bookreviewex/presentation/MainActivity.kt
@@ -1,66 +1,96 @@
 package com.example.bookreviewex.presentation
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.core.view.isVisible
+import androidx.core.widget.doAfterTextChanged
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.bookreviewex.databinding.ActivityMainBinding
 import com.example.bookreviewex.presentation.viewmodel.BookListState
 import com.example.bookreviewex.presentation.viewmodel.BooksViewModel
 import com.example.bookreviewex.presentation.viewmodel.ReviewListState
-import com.example.bookreviewex.repository.localdb.entity.ReviewEntity
-import com.example.bookreviewex.repository.service.model.BookDTO
-import com.example.bookreviewex.usecase.InsertReviewListUseCase
 import com.example.bookreviewex.presentation.viewmodel.ReviewListViewModel
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
+import com.example.bookreviewex.repository.service.model.BookDTO
 import org.koin.android.ext.android.inject
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding:ActivityMainBinding
     private val reviewListViewModel:ReviewListViewModel by inject()
-    private val booksViewModel : BooksViewModel by inject()
+    private val bookListViewModel : BooksViewModel by inject()
 
-    private val adapter = BooksAdapter()
+    private val reviewAdapter = ReviewAdapter()
+    private val bookAdapter = BooksAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        attachListView()
+        initViews()
         observeData()
+    }
 
-        binding.searchEditTextView.handleIconClick = handleSearchButton
+    private fun initViews(){
+
+        binding.bookListView.layoutManager = LinearLayoutManager(this)
+        binding.bookListView.adapter = bookAdapter
+        bookAdapter.submitList(null)
+
+        binding.reviewListView.layoutManager = LinearLayoutManager(this)
+        binding.reviewListView.adapter = reviewAdapter
+        reviewAdapter.submitList(null)
+
+        binding.searchEditTextView.handleIconClick = handleSearchButtonClick
+
+        handleSearchTextViewClick()
+        handleSearchTextViewTextChange()
 
     }
 
-    private fun attachListView(){
-        binding.bookList.layoutManager = LinearLayoutManager(this)
-        binding.bookList.adapter = adapter
-        adapter.submitList(null)
+    private fun handleSearchTextViewTextChange(){
+        binding.searchEditTextView.doAfterTextChanged {
+            if(it?.length?:0 <=0) changeVisibleListView(false)
+        }
 
+    }
+
+    private fun changeVisibleListView( bookListViewVisible :Boolean){
+        if(bookListViewVisible.not()){
+            bookAdapter.submitList(null)
+            bookAdapter.notifyDataSetChanged()
+        }
+
+        binding.bookListView.isVisible = bookListViewVisible
+        binding.reviewListView.isVisible = !bookListViewVisible
+    }
+
+    private fun handleSearchTextViewClick(){
+        binding.searchEditTextView.setOnClickListener {
+            changeVisibleListView(true)
+        }
     }
 
     private fun observeData(){
-//        reviewListViewModel.reviewList.observe(this){
-//            Log.d(TAG, "state : ${it}")
-//
-//            when(it){
-//                is ReviewListState.Error -> handleErrorState()
-//                is ReviewListState.Idle -> attachListView()
-//                is ReviewListState.Loading -> handleLoadingState()
-//                is ReviewListState.Success -> handleSuccessState(it)
-//            }
-//        }
+        reviewListViewModel.reviewList.observe(this){
+            Log.d(TAG, "state : ${it}")
 
-        booksViewModel.book.observe(this){
+            when(it){
+                is ReviewListState.Error -> handleErrorState()
+                is ReviewListState.Idle -> changeVisibleListView(false)
+                is ReviewListState.Loading -> handleLoadingState()
+                is ReviewListState.Success -> handleReviewListSuccessState(it)
+            }
+        }
+
+        bookListViewModel.book.observe(this){
             Log.d(TAG, "state : ${it}")
             when(it){
-                BookListState.Idle -> attachListView()
+                BookListState.Idle -> changeVisibleListView(true)
                 BookListState.Loading -> handleLoadingState()
-                is BookListState.Success -> handleSuccessState(it)
+                is BookListState.Success -> handleBookListSuccessState(it)
             }
         }
 
@@ -75,18 +105,18 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    private fun handleSuccessState(state:BookListState.Success){
-//        adapter.submitList(state.reviewList.toMutableList())
-
-        adapter.submitList(state.books.toMutableList())
+    private fun handleReviewListSuccessState(state: ReviewListState.Success){
+        reviewAdapter.submitList(state.reviewList.toMutableList())
     }
 
+    private fun handleBookListSuccessState(state:BookListState.Success){
+        changeVisibleListView(true)
+        bookAdapter.submitList(state.books.toMutableList())
+    }
 
-
-    private val handleSearchButton:()->Unit = {
+    private val handleSearchButtonClick:()->Unit = {
         val keyword = binding.searchEditTextView.text.toString()
-        //todo 도서 검색 api로 요청하기
-        booksViewModel.searchBook(keyword)
+        bookListViewModel.searchBook(keyword)
     }
 
     companion object{
